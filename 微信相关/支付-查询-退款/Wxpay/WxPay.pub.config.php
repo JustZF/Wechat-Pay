7635cb53529b80d01c595926f4c0ffcc837135de
Just <5210239+zzf199407@user.noreply.gitee.com>
2020-05-08 15:43:24 +0800

    update 微信相关/支付-查询-退款/Wxpay/WxPay.pub.config.php.

--- 微信相关/支付-查询-退款/Wxpay/WxPay.pub.config.php
+++ 微信相关/支付-查询-退款/Wxpay/WxPay.pub.config.php
@@ -7,17 +7,14 @@ class WxPayConf_pub
 {
 	//=======【基本信息设置】=====================================
 	//微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
-	const APPID = 'wxbfb93569bee3f4f3';
+	const APPID = '';
 	//受理商ID，身份标识
-	//const MCHID = '1500989771';
-	const MCHID = '1526323121';
-    //const MCHID = '1524099021';
+	const MCHID = '';
 
 	//商户支付密钥Key。审核通过后，在微信发送的邮件中查看
-	//const KEY = 'bvibotwgm1hqj0ojxannvxhwcia0jk5v';
-	const KEY = 'jJYRpYDIl9MptqdenPfTQyrGSkCc2QjJ';
+	const KEY = '';
 	//JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
-	const APPSECRET = '6c57a4daa097051c6cc914195adcb312';
+	const APPSECRET = '';
 	
 	const SSLCERT_PATH = '../cacert/apiclient_cert.pem';
 	const SSLKEY_PATH = '../cacert/apiclient_key.pem';
